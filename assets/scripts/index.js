976a723f98c922f5ac54ece6c441168beb21e422
Nicolas <76515190+NicolasMB3@users.noreply.github.com>
2022-11-05 09:51:33 +0100

    Some fix :+1:

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,13 +1,22 @@
 // Display modal for "entrejambe" section
-const buttonEntrejambe = document.getElementById('modalEntrejambe');
+const buttonEntrejambe = document.getElementsByClassName('modalEntrejambe');
+const validBtn = document.getElementsByClassName('vtmn-btn_size--stretched')
 const modal = document.getElementById('section--modal');
 const closeModalBtn = document.getElementsByClassName('btnClose');
 
-if (!buttonEntrejambe == false) {
-   buttonEntrejambe.addEventListener('click', () => {
-      modal.style.display = 'flex';
-   });
-}
+[...validBtn].forEach(element => {
+   element.addEventListener('click', () => {
+      modal.style.display = 'none';
+   })
+});
+
+[...buttonEntrejambe].forEach(element => {
+   if (!element == false) {
+      element.addEventListener('click', () => {
+         modal.style.display = 'flex';
+      });
+   }
+});
 
 for (let i = 0; i < closeModalBtn.length; i++) {
    closeModalBtn[i].addEventListener('click', () => {
--- assets/scripts/pratique.js
+++ assets/scripts/pratique.js
@@ -1,13 +1,22 @@
 // Display modal for "entrejambe" section
-const informationBtn = document.getElementById('more-information--btn');
+const informationBtn = document.getElementsByClassName('more-information--btn');
 const modal = document.getElementById('pratique--modal');
 const closeModalBtn = document.getElementsByClassName('btnClose');
+const validBtnPrat = document.getElementsByClassName('vtmn-btn_size--stretched');
 
-if (!informationBtn == false) {
-   informationBtn.addEventListener('click', () => {
-      modal.style.display = 'flex';
-   });
-}
+[...validBtnPrat].forEach(element => {
+   element.addEventListener('click', () => {
+      modal.style.display = 'none';
+   })
+});
+
+[...informationBtn].forEach(element => {
+   if (!element == false) {
+      element.addEventListener('click', () => {
+         modal.style.display = 'flex';
+      });
+   }
+});
 
 for (let i = 0; i < closeModalBtn.length; i++) {
    closeModalBtn[i].addEventListener('click', () => {
@@ -16,3 +25,25 @@ for (let i = 0; i < closeModalBtn.length; i++) {
       }
    })
 }
+
+// Display cards with select value
+const getCards = document.getElementsByClassName('card--container');
+const selectVelo = document.getElementById('velo');
+const confortCard = document.getElementById('confort');
+const perfCard = document.getElementById('perf');
+
+[...getCards].forEach(element => {
+   element.style.display = 'none';
+});
+
+console.log(selectVelo)
+
+selectVelo.addEventListener("change", () => {
+   if(selectVelo.value == 'chemin') {
+      confortCard.style.display = 'block';
+      perfCard.style.display = 'none';
+   } else {
+      confortCard.style.display = 'none';
+      perfCard.style.display = 'block';
+   }
+});
